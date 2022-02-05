32ea2abf86cef2d9c82ab9341d6370e60f7de835
AbrarMahabub21 <abrar.nowrid@gmail.com>
2022-02-05 21:35:10 +0600

    added Bossman

---
+++ bossman/app/src/main/java/com/example/bossman/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.bossman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bossman man = new bossman(21 ,  "Nowrid");
+
+         Log.d(TAG, "onCreate: " + man.age);
+         Log.d(TAG, "onCreate: " + man.name );
+    }
+}
---
+++ bossman/app/src/main/java/com/example/bossman/bossman.java
@@ -0,0 +1,18 @@
+package com.example.bossman;
+
+public class bossman {
+    int age ;
+    String name;
+    public bossman(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+
+
+
+
+
+
+
+}
